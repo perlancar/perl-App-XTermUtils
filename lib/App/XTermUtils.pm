bf3a846007481f5d19209428a1848adc07c00a00
perlancar (on pc-office) <perlancar@gmail.com>
2018-09-26 22:26:45 +0700

    Move Rinci metadata to XTerm::Util, use that directly

--- lib/App/XTermUtils.pm
+++ lib/App/XTermUtils.pm
@@ -3,45 +3,6 @@ package App::XTermUtils;
 # DATE
 # VERSION
 
-use 5.010001;
-use strict;
-use warnings;
-
-our %SPEC;
-
-$SPEC{get_term_bgcolor} = {
-    v => 1.1,
-    summary => 'Get terminal background color',
-    args => {},
-};
-sub get_term_bgcolor {
-    require XTerm::Util;
-
-    my %args = @_;
-
-    [200, "OK", XTerm::Util::get_term_bgcolor()];
-}
-
-$SPEC{set_term_bgcolor} = {
-    v => 1.1,
-    summary => 'Set terminal background color',
-    args => {
-        rgb => {
-            schema => 'color::rgb24*',
-            req => 1,
-            pos => 0,
-        },
-    },
-};
-sub set_term_bgcolor {
-    require XTerm::Util;
-
-    my %args = @_;
-
-    XTerm::Util::set_term_bgcolor($args{rgb});
-    [200, "OK"];
-}
-
 1;
 #ABSTRACT: Utilities related to XTerm
 
